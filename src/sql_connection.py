444ced1ba6896a1aa5e380fa269782fcfb74cfbe
goldenserzh <serzh.golden@gmail.com>
2026-05-06 15:04:53 +0700

    fixing connection

--- src/sql_connection.py
+++ src/sql_connection.py
@@ -15,18 +15,24 @@ DBNAME = os.getenv("DBNAME")
 try:
     db_url = f'postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}/{DBNAME}'
     engine = create_engine(db_url, echo=True)
-    
+
 except:
     raise ValueError("Нет подулючения к бд")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def making_to_sql():
-    pass
+def making_to_sql(df: pd.DataFrame, table_name: str):
+    df.to_sql(
+        name=table_name,
+        con=engine,
+        if_exists='replace',
+        index=False,
+        chunksize=1000
+    )
 
 
-def upload_csv(csv_path: str, table_name:str):
-    df_train = pd.read_csv("Путь до triain_df")
-    df_test = pd.read_csv('Путь до test_df')
-    df_
+def upload_csv(train_path: str, test_path: str):
+    making_to_sql(pd.read_csv(train_path), table_name='cars_train')
+    making_to_sql(pd.read_csv(test_path),  table_name='cars_test')
+
 
