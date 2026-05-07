4e55a3aa19ddb3a2649e71050929da67bbefa3f1
goldenserzh <serzh.golden@gmail.com>
2026-05-07 16:49:54 +0700

    fixing sql-connection

--- src/sql_connection.py
+++ src/sql_connection.py
@@ -1,23 +1,20 @@
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
-from models import Base
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import pandas as pd
 
-load_dotenv(dotenv_path = '...')
+load_dotenv()
 
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 HOST = os.getenv("HOST")
 DBNAME = os.getenv("DBNAME")
 
-try:
-    db_url = f'postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}/{DBNAME}'
-    engine = create_engine(db_url, echo=True)
 
-except:
-    raise ValueError("Нет подулючения к бд")
+db_url = f'postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}/{DBNAME}'
+engine = create_engine(db_url, echo=True)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -34,5 +31,19 @@ def making_to_sql(df: pd.DataFrame, table_name: str):
 def upload_csv(train_path: str, test_path: str):
     making_to_sql(pd.read_csv(train_path), table_name='cars_train')
     making_to_sql(pd.read_csv(test_path),  table_name='cars_test')
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    upload_csv('train_df_path', 'test_df_path')
+
+        
+
 
 
